cc02277409a55a3128685af6324874d9b268befd
Akeymurth <arkeyto@gmail.com>
2025-05-09 01:36:44 -0600

    class5

--- src/02-Tuples.ts
+++ src/02-Tuples.ts
@@ -5,7 +5,21 @@ prices.push("four thousand");
 
 console.log(prices);
 
+let user: [string, number, boolean];
 
+user = ["John", 20, true];
+
+// en este caso, si ponemos un tercer elemento, nos da un error
+// user = ["John", 20, true, "admin"];
+
+// si ponemos un tercer elemento, pero es de otro tipo, nos da un error
+// user = ["John", 20, "admin"];
+
+const [username, age] = user;
+
+console.log(username, age); 
+
+//en este caso, como la constante username como es una dupla solo toma los dos primeros elementos
 
 
 
